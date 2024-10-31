59a42d9a51bf03bc8055af5201f99d2c7af289a0
zapotz <victorzptzu@gmail.com>
2024-10-31 14:11:59 -0600

    cambio de keys

--- .history/infra/provider_20241030084938.tf
+++ .history/infra/provider_20241030084938.tf
@@ -1,5 +1,4 @@
 provider "aws" {
   region                   = "eu-central-1"
   shared_credentials_files = ["/home/victor-zapotz/.aws/credentials"]
-  profile = "default"
 }
--- .history/infra/provider_20241031133353.tf
+++ .history/infra/provider_20241031133353.tf
@@ -1,5 +1,5 @@
 provider "aws" {
   region                   = "eu-central-1"
   shared_credentials_files = ["/home/victor-zapotz/.aws/credentials"]
-  profile = "default"
+  profile = "vscode"
 }
