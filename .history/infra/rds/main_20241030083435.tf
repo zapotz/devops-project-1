1a049a94abc43b3a30b033807cb6df11240333b6
zapotz <victorzptzu@gmail.com>
2024-10-31 16:16:37 -0600

    ajustes3

--- .history/infra/rds/main_20241030083435.tf
+++ .history/infra/rds/main_20241030083435.tf
@@ -17,7 +17,7 @@ resource "aws_db_instance" "default" {
   storage_type            = "gp2"
   engine                  = "mysql"
   engine_version          = "5.7"
-  instance_class          = "db.t3.micro"
+  instance_class          = "db.t2.micro"
   identifier              = var.mysql_db_identifier
   username                = var.mysql_username
   password                = var.mysql_password
